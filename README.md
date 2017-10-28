f87f231bbf75bd07b4f6242848001cde0a8de7d6
Kyle Ueyama <khueyama@gmail.com>
2017-10-28 15:55:31 -0400

    add initial plot

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # virginia-gubernatorial-2017
+
+![](/img/2013_2016.png)
--- r/plot.R
+++ r/plot.R
@@ -1,3 +1,6 @@
+load('data/df_results.Rdata')
+load('data/df_registered.Rdata')
+
 df <- df_results %>% 
   select(locality, year, percent) %>% 
   spread(year, percent) %>%
@@ -10,5 +13,9 @@ ggplot(df, aes(`2013`, `2016`, size = active, color = `2016` - `2013`)) +
   scale_x_continuous(limit = c(0, 1), labels = scales::percent) +
   scale_y_continuous(limit = c(0, 1), labels = scales::percent) +
   theme_minimal() +
-  theme(legend.position="none")
+  theme(legend.position="none") +
+  labs(title = 'Democratic Two-Party Vote Share', 
+          subtitle = '2016 vs 2013 General Election',
+          caption = "Data via Virginia Department of Elections")
   
+ggsave("img/2013_2016.png")
